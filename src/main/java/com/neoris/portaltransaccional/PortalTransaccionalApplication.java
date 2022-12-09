6d1c9edbcf55bd70e55a2eb8b38e343def5f4637
Gustavo Galvis <ggalvis216@gmail.com>
2022-12-08 23:03:11 -0500

    Spring initializr

---
+++ src/main/java/com/neoris/portaltransaccional/PortalTransaccionalApplication.java
@@ -0,0 +1,13 @@
+package com.neoris.portaltransaccional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalTransaccionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalTransaccionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neoris/portaltransaccional/PortalTransaccionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neoris.portaltransaccional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalTransaccionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
